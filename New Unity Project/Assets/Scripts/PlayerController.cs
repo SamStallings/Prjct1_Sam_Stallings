fc4d0568d11b4778d455940e1f7e745b204793c7
Sam Stallings <10915865@my.uvu.edu>
2021-10-14 12:27:32 -0600

    Added Unity Project 1
    collisions, controls, camera, setup, assets, etc.

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move the vehicle forward on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        //Rotate the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
